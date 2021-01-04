9f52e32babcdfae5a1c081c45e317ac8a2b6669a
CJ Grady <cjgrady@gmail.com>
2021-01-04 10:32:25 -0600

    Fixed authentication for python3

--- lm_client/apis/auth.py
+++ lm_client/apis/auth.py
@@ -1,11 +1,9 @@
 """Module containing authentication service calls
 """
-try:  # pragma: no cover
-    import http.cookiejar as cookie_jar
-    from urllib.parse import urlparse
-except:  # pragma: no cover
-    import cookielib as cookie_jar
-    from urlparse import urlparse
+import http.cookiejar as cookie_jar
+from urllib.parse import urlparse
+import urllib.request
+
 import json
 
 from lm_client.common.api_service import ApiService
@@ -23,12 +21,12 @@ class AuthApiService(ApiService):
         policy = cookie_jar.DefaultCookiePolicy(
             allowed_domains=(policy_server,))
         self.cookie_jar = cookie_jar.LWPCookieJar(policy=policy)
-        opener = urllib2.build_opener(
-            urllib2.HTTPCookieProcessor(self.cookie_jar))
-        urllib2.install_opener(opener)
+        opener = urllib.request.build_opener(
+            urllib.request.HTTPCookieProcessor(self.cookie_jar))
+        urllib.request.install_opener(opener)
 
         response = self.api_client.post(
-            'api/v2/login', user_id=user_id, pword=passwd)
+            'api/login', user_id=user_id, pword=passwd)
         return response
 
     # ...........................
